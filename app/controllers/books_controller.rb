eeafc7f493f1391207d90ab739f97e6530333310
Kanta Nakazawa <109840989+webcampkan@users.noreply.github.com>
2022-08-11 02:48:47 +0000

    [Add]バリデーション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,16 +4,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id), flash:{notice: 'Book was successfully created.'}
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), flash:{notice: 'Book was successfully created.'}
     else
-      redirect_to '/books'
+      @books=Book.all
+      render :index
     end
   end
 
   def index
     @books=Book.all
+    @book=Book.new
   end
 
   def show
@@ -25,10 +27,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book.id), flash:{notice: 'Book was successfully updated.'}
-  end
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), flash:{notice: 'Book was successfully updated.'}
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -39,6 +43,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
